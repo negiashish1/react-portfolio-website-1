23f0a7031bc2d2eb11d2e0a3e4e7b27a55a39833
negiashish1 <negi.ashish012@gmail.com>
2022-09-25 01:18:59 +0530

    added data.js

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -5,7 +5,7 @@ import AVTR2 from '../../assets/avatar2.jpg'
 import AVTR3 from '../../assets/avatar3.jpg'
 import AVTR4 from '../../assets/avatar4.jpg'
 
-// import Swiper core and required modules
+// import Swiper core and required modules 
 import { Pagination } from 'swiper';
 
 import { Swiper, SwiperSlide } from 'swiper/react/swiper-react';
